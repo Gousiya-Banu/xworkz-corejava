05d90eaeadfad481bebc61ac1d6a0159d10c3b9d
gousiya-Banu <gousiyaece.rymec@gmail.com>
2020-11-10 11:41:18 +0530

    Adding : ByteDataType

---
+++ ByteDataType.java
@@ -0,0 +1,14 @@
+class Byte
+{
+      public static void main(String a[])
+	  {
+	      byte noofchairs=5;
+		  byte age=21;
+		  byte height=5;
+		  
+		  System.out.println(noofchairs +"\n");
+		  System.out.println(age +"\n");
+		  System.out.println(height);
+		 
+	  }
+}
